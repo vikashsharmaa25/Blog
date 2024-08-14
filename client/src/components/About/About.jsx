ee819da36fb9136423875d8a3ba7c2c9a0de6fa1
Vikash Sharma <Vikash9422@gmail.com>
2024-08-14 12:38:15 +0530

    almost final push

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 function About() {
   return (
     <div>
-      <div className="min-h-screen bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500">
+      <div className="min-h-screen bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 flex justify-center items-center">
         <Link to={"https://vikashsharma.vercel.app/"}>
-          <button className="text-xl px-5 py-1 rounded-md text-white font-serif bg-indigo-600">
+          <button className="text-2xl px-10 py-2 rounded-md text-white font-serif bg-indigo-600">
             Portfolio
           </button>
         </Link>
