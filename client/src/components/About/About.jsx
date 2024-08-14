fcb1499b4a89e8b4848098d0d5a3c6502dd3ce6c
Vikash Sharma <Vikash9422@gmail.com>
2024-08-14 12:33:40 +0530

    portfolio link

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function About() {
   return (
     <div>
       <div className="min-h-screen bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500">
-        <button className="text-xl px-5 py-1 rounded-md text-white font-serif bg-indigo-600">
-          Portfolio
-        </button>
+        <Link to={"https://vikashsharma.vercel.app/"}>
+          <button className="text-xl px-5 py-1 rounded-md text-white font-serif bg-indigo-600">
+            Portfolio
+          </button>
+        </Link>
       </div>
     </div>
   );
